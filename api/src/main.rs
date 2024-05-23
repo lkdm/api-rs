e4782992b5ecddb63ff5845f1dd1af5dc88c1951
Luke M <44519853+lkdm@users.noreply.github.com>
2024-05-23 16:16:12 +1000

    feat: create basic http server.

---
+++ api/src/main.rs
@@ -0,0 +1,38 @@
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::routing::get;
+use axum::Json;
+use serde_json::json;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    // Start tracing.
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // TODO: Create connection pool here, to pass into app.
+
+    let app = axum::Router::new()
+        .fallback(fallback)
+        .route("/status/", get(status));
+
+    // Run our application as a hyper server on http://localhost:3000.
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+// axum handler for any request that fails to match the router routes.
+// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (
+        axum::http::StatusCode::NOT_FOUND,
+        format!("No route {}", uri),
+    )
+}
+
+async fn status() ->  Result<impl IntoResponse, StatusCode> {
+    Ok("Ok!")
+}
